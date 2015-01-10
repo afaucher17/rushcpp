4dcbc23044d598b4df0b1a0f3f2d59f4f6c58f58
Aymeric FAUCHER <afaucher@e3r10p10.42.fr>
2015-01-10 21:56:59 +0100

    private => protected

--- AGameEntity.hpp
+++ AGameEntity.hpp
@@ -1,18 +1,23 @@
 #ifndef AGAMEENTITY_HPP
 # define AGAMEENTITY_HPP
 
+#include "Position.hpp"
+#include "Hitbox.hpp"
+#include "Sprite.hpp"
+#include <iostream>
+#include <ssstream>
 class AGameEntity
 {
 	public:
-		AGameEntity(int x, int y, Sprite const & sprite);
+		AGameEntity(int x, int y, int wdth, int hght, int speed, int maxspeed, Sprite const & sprite);
 		AGameEntity(AGameEntity const & src);
 		virtual ~AGameEntity( void );
 
 		AGameEntity &			operator=(AGameEntity const & rhs);
 
-		Position &				getPosition(void) const;
-		Hitbox &				getHitbox(void) const;
-		Sprite &				getSprite(void) const;
+		Position const &		getPosition(void) const;
+		Hitbox const &			getHitbox(void) const;
+		Sprite const &			getSprite(void) const;
 		int						getSpeed() const;
 
 		virtual void			refresh() = 0;
@@ -21,15 +26,17 @@ class AGameEntity
 		std::string				toString(void) const;
 
 	protected:
+		AGameEntity( void );
 		Position				_pos;
 		Hitbox					_hb;
 		Sprite					_sp;
 		int						_dead;
 		int						_speed;
 		int						_maxspeed;
+		int						_index;
 
 	private:
-		AGameEntity( void );
+		static unsigned int		_nb_inst;
 };
 
 std::ostream &					operator<<(std::ostream & o, AGameEntity const & rhs);
