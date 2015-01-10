f00c4fd3defa32b6aababbbe84782f7e70fdf5da
Thomas DIEUMEGARD <tdieumeg@e3r10p9.42.fr>
2015-01-10 15:05:38 +0100

    class Window

---
+++ Window.hpp
@@ -0,0 +1,26 @@
+#ifndef WINDOW_HPP
+# define WINDOW_HPP
+
+class Window
+{
+	public:
+		Window(int const x, int const y);
+		Window(Window const & src);
+		~Window( void );
+
+		Window &			operator=(Window const & rhs);
+
+		void				initWindow(void);
+		void				refreshWindow(void);
+
+		int					getX(void) const;
+		int					getY(void) const;
+
+	private:
+		Window( void );
+
+		int					_x;
+		int					_y;
+};
+
+#endif /* WINDOW_HPP */
